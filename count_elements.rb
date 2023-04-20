234183fb4af3fc6433d5aab6585cee32a10ca55b
WD1 <wd1ror1@yahoo.com>
2023-04-20 18:59:43 +0900

    Test Complete

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,8 @@
 class CountElements
-
+	def initialize(results = [])
+	    @results = results
+	end
+	def perform
+	    @results.inject(Hash.new(0)) { |hash, e| hash[e] += 1 ; hash }
+	end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,16 @@
 class ValidatePassword
-
+	def initialize(password = '')
+	    @password = password
+	end
+	def perform
+	    if @password =~/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*+=:;?<>])[A-Za-z\d!@#$%&*+=:;?<>]{6,24}$/
+	    	if @password.match?('((.)\2{2,})')
+	    		false
+	    	else
+	    		true
+	    	end
+	    else
+	     	false
+	    end 
+	end
 end
